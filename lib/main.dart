e25c231f93d411d24e07993c43a0648ba92f9b55
Harishan15 <38322265+Harishan15@users.noreply.github.com>
2024-01-13 10:44:10 +0530

    updated test practices

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo by Harishan',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page by harishan'),
     );
   }
 }
@@ -39,6 +40,8 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  int bottomNavBarIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,12 +54,14 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Hello World, You have pushed the button this many times:',
+              'Hello World, You have pushed the test',
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            ElevatedButton(
+                onPressed: () => print('Button Clicked'), child: Text('test'))
           ],
         ),
       ),
@@ -64,6 +69,26 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Rides',
+            icon: Icon(Icons.bike_scooter),
+          ),
+          BottomNavigationBarItem(
+            label: 'Setting',
+            icon: Icon(Icons.settings),
+          )
+        ],
+        currentIndex: bottomNavBarIndex,
+        onTap: (int index) => setState(() {
+          bottomNavBarIndex = index;
+        }),
       ),
     );
   }
